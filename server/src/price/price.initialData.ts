391733029ab817d6cac8209aa4b0fb9ab771e5ab
Захаров Михаил <zakharov@compel.ru>
2026-01-01 22:45:23 +0500

    Update Price InitialData

--- server/src/price/price.initialData.ts
+++ server/src/price/price.initialData.ts
@@ -5,8 +5,8 @@ export const priceInitialData: PriceCreationAttrs[] = [
   { id: 2, companyId: 1, value: 300, discountImpact: true },
   { id: 3, companyId: 1, value: 400, discountImpact: true },
   { id: 4, companyId: 1, value: 500, discountImpact: true },
-  { id: 5, companyId: 1, value: 10, discountImpact: true },
-  { id: 6, companyId: 1, value: 20, discountImpact: true },
-  { id: 7, companyId: 1, value: 30, discountImpact: true },
-  { id: 8, companyId: 1, value: 40, discountImpact: true },
+  { id: 5, companyId: 1, value: 10, discountImpact: false },
+  { id: 6, companyId: 1, value: 20, discountImpact: false },
+  { id: 7, companyId: 1, value: 30, discountImpact: false },
+  { id: 8, companyId: 1, value: 40, discountImpact: false },
 ];
