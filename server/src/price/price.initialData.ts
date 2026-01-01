8f3c0a1604aa4956160ecb2d10b38f37fefbe87f
Захаров Михаил <zakharov@compel.ru>
2026-01-01 22:44:31 +0500

    Update Price InitialData

--- server/src/price/price.initialData.ts
+++ server/src/price/price.initialData.ts
@@ -1,5 +1,12 @@
 import { PriceCreationAttrs } from './price.model';
 
 export const priceInitialData: PriceCreationAttrs[] = [
-  { companyId: 1, value: 200, discountImpact: true },
+  { id: 1, companyId: 1, value: 200, discountImpact: true },
+  { id: 2, companyId: 1, value: 300, discountImpact: true },
+  { id: 3, companyId: 1, value: 400, discountImpact: true },
+  { id: 4, companyId: 1, value: 500, discountImpact: true },
+  { id: 5, companyId: 1, value: 10, discountImpact: true },
+  { id: 6, companyId: 1, value: 20, discountImpact: true },
+  { id: 7, companyId: 1, value: 30, discountImpact: true },
+  { id: 8, companyId: 1, value: 40, discountImpact: true },
 ];
--- server/src/price/priceParametersOptionConditions.initialData.ts
+++ server/src/price/priceParametersOptionConditions.initialData.ts
@@ -3,5 +3,13 @@ import { PriceParametersOptionConditionsCreationAttrs } from './priceParametersO
 export const priceParametersOptionConditionsInitialData: PriceParametersOptionConditionsCreationAttrs[] =
   [
     { priceId: 1, optionId: 22 },
-    { priceId: 1, optionId: 23 },
+    { priceId: 2, optionId: 23 },
+    { priceId: 3, optionId: 23 },
+    { priceId: 3, optionId: 17 },
+    { priceId: 4, optionId: 23 },
+    { priceId: 4, optionId: 21 },
+    { priceId: 5, optionId: 26 },
+    { priceId: 6, optionId: 27 },
+    { priceId: 7, optionId: 28 },
+    { priceId: 8, optionId: 29 },
   ];
