0c66d42780d52e6a35953ea89c621225add51a92
ZakharovaTatiana <scriptanya@gmail.com>
2026-01-17 16:25:56 +0500

    UI // logo

--- client/src/widgets/logo/index.tsx
+++ client/src/widgets/logo/index.tsx
@@ -4,8 +4,8 @@ import { Tire } from "@shared/ui/Icons/Tire.tsx";
 export const Logo = () => {
   return (
     <Link to={"/"} title={"Главная"} className={"logo"}>
-      <Tire className="h-10 w-10 text-green-800 dark:text-white" />
-      <span className={"uppercase text-3xl tracking-tight"}>Tire service</span>
+      <Tire className="h-8 w-8 mr-1 text-green-800 dark:text-white" />
+      <span className={"uppercase font-bold text-4xl tracking-wide outlined-heading"}>Tire service</span>
     </Link>
   );
 };
